630de974c63a92e81d9e7dc99d2cf452de7f1f4f
Mark Tomm <mark.tomm@martem.ee>
2023-01-31 18:07:00 +0200

    meson build file formatting

---
+++ meson_fmt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function is-app-installed(){
+    hash $1 2>&-
+    return $?
+}
+
+is-app-installed muon || {
+    echo "You don't have muon installed. Visit https://muon.build/"
+    exit 1
+}
+
+muon fmt -i meson.build 
+muon fmt -i enum/meson.build 
+muon fmt -i oop/meson.build 
+muon fmt -i visitor/meson.build 
+muon fmt -i type_erasure_up/meson.build 
+muon fmt -i variant_t/meson.build 
+muon fmt -i strategy/meson.build 
