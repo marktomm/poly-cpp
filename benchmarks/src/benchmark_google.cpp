0b40ab0defdeafd2e399d49b16c33cec32fa7e82
Mark Tomm <mark.tomm@martem.ee>
2023-02-18 10:53:16 +0200

    add to google benchmarks

--- benchmarks/src/benchmark_google.cpp
+++ benchmarks/src/benchmark_google.cpp
@@ -1,205 +1,16 @@
 #include <benchmark/benchmark.h>
 
-#include "enum/include/dev_ctx.h"
-#include "enum/include/dev_factory.h"
-#include "oop/include/dev_ctx.h"
-#include "oop/include/dev_factory.h"
-#include "visitor/include/dev_ctx.h"
-#include "visitor/include/dev_factory.h"
-
-#include <algorithm>
-#include <cstdint>
-#include <random>
-#include <vector>
+#include "common.h"
 
 constexpr static uint32_t roundCount = 1;
-
-// static std::vector<std::unique_ptr<Enum::Port> > EnumPortsInit()
-// {
-//     using namespace Enum;
-//     using Ports = std::vector<std::unique_ptr<Port> >;
-
-//     Ports ports;
-//     for (uint32_t i = 0; i < 50; ++i) {
-//         ports.push_back(createTcpPort("localhost", 2404));
-//     }
-//     for (uint32_t i = 0; i < 50; ++i) {
-//         ports.push_back(createSerialPort("/dev/ttyUSB0"));
-//     }
-
-//     return ports;
-// }
-
-// static std::vector<std::unique_ptr<Enum::Port> > EnumPortsInitInterleave()
-// {
-//     using namespace Enum;
-//     using Ports = std::vector<std::unique_ptr<Port> >;
-
-//     Ports ports;
-//     for (uint32_t i = 0; i < 50; ++i) {
-//         ports.push_back(createTcpPort("localhost", 2404));
-//         ports.push_back(createSerialPort("/dev/ttyUSB0"));
-//     }
-
-//     return ports;
-// }
-
-// this function guarantees same randomness for all benchmark cases
-static std::vector<uint32_t> setup()
-{
-    static bool setupDone = false;
-    static std::vector<uint32_t> v(100);
-    if (setupDone) {
-        return v;
-    }
-    std::random_device rd;
-    std::mt19937 generator(rd());
-    std::generate(v.begin(), v.end(), [n = 0]() mutable { return n++; });
-    std::shuffle(v.begin(), v.end(), generator);
-    setupDone = true;
-    return v;
-}
-
-static std::vector<std::unique_ptr<Enum::Port> > EnumPortsInitRandom()
-{
-    std::vector<uint32_t> v = setup();
-    using namespace Enum;
-    using Ports = std::vector<std::unique_ptr<Port> >;
-
-    Ports ports(100);
-    for (uint32_t i = 0; i < 50; ++i) {
-        ports[v[i]] = createTcpPort("localhost", 2404);
-    }
-    for (uint32_t i = 50; i < 100; ++i) {
-        ports[v[i]] = createSerialPort("/dev/ttyUSB0");
-    }
-
-    return ports;
-}
-
-static std::vector<std::unique_ptr<Oop::Port> > OopPortsInitRandom()
-{
-    std::vector<uint32_t> v = setup();
-
-    using namespace Oop;
-    using Ports = std::vector<std::unique_ptr<Port> >;
-
-    Ports ports(100);
-    for (uint32_t i = 0; i < 50; ++i) {
-        ports[v[i]] = createTcpPort("localhost", 2404);
-    }
-    for (uint32_t i = 50; i < 100; ++i) {
-        ports[v[i]] = createSerialPort("/dev/ttyUSB0");
-    }
-
-    return ports;
-}
-
-static std::vector<std::unique_ptr<Visit::Port> > VisitPortsInitRandom()
-{
-    std::vector<uint32_t> v = setup();
-
-    using namespace Visit;
-    using Ports = std::vector<std::unique_ptr<Port> >;
-
-    Ports ports(100);
-    for (uint32_t i = 0; i < 50; ++i) {
-        ports[v[i]] = createTcpPort("localhost", 2404);
-    }
-    for (uint32_t i = 50; i < 100; ++i) {
-        ports[v[i]] = createSerialPort("/dev/ttyUSB0");
-    }
-
-    return ports;
-}
-
-// static void BM_Enum(benchmark::State& state)
-// {
-//     // Perform setup here
-//     using namespace Enum;
-//     using Ports = std::vector<std::unique_ptr<Port> >;
-//     Ports ports = EnumPortsInit();
-//     for (auto _ : state) {
-//         // This code gets timed
-//         for (uint32_t i = 0; i < roundCount; ++i)
-//             writePorts(ports, 0xFF);
-//     }
-// }
-
-// static void BM_Enum2(benchmark::State& state)
-// {
-//     // Perform setup here
-//     using namespace Enum;
-//     using Ports = std::vector<std::unique_ptr<Port> >;
-//     Ports ports = EnumPortsInit();
-//     for (auto _ : state) {
-//         benchmark::DoNotOptimize(ports.data());
-//         // This code gets timed
-//         for (uint32_t i = 0; i < roundCount; ++i)
-//             writePorts(ports, 0xFF);
-//         benchmark::ClobberMemory();
-//     }
-// }
-
-// static void BM_Enum3(benchmark::State& state)
-// {
-//     // Perform setup here
-//     using namespace Enum;
-//     using Ports = std::vector<std::unique_ptr<Port> >;
-//     Ports ports = EnumPortsInit();
-//     for (auto _ : state) {
-//         benchmark::DoNotOptimize(ports.data());
-//         // This code gets timed
-//         for (uint32_t i = 0; i < roundCount; ++i) {
-//             writePorts(ports, 0xFF);
-//             benchmark::ClobberMemory();
-//         }
-//     }
-// }
-
-// static void BM_EnumInterleave(benchmark::State& state)
-// {
-//     // Perform setup here
-//     using namespace Enum;
-//     using Ports = std::vector<std::unique_ptr<Port> >;
-//     Ports ports = EnumPortsInitInterleave();
-//     for (auto _ : state) {
-//         // This code gets timed
-//         for (uint32_t i = 0; i < roundCount; ++i)
-//             writePorts(ports, 0xFF);
-//     }
-// }
-
-// static void BM_EnumInterleave2(benchmark::State& state)
-// {
-//     // Perform setup here
-//     using namespace Enum;
-//     using Ports = std::vector<std::unique_ptr<Port> >;
-//     Ports ports = EnumPortsInitInterleave();
-//     for (auto _ : state) {
-//         // This code gets timed
-//         benchmark::DoNotOptimize(ports.data());
-//         for (uint32_t i = 0; i < roundCount; ++i) {
-//             writePorts(ports, 0xFF);
-//             benchmark::ClobberMemory();
-//         }
-//     }
-// }
-
-static void BM_Setup(benchmark::State& state)
-{
-    for (auto _ : state) {
-        std::vector<uint32_t> v = setup();
-        benchmark::DoNotOptimize(v.data());
-    }
-}
 
 static void BM_EnumRandom(benchmark::State& state)
 {
     // Perform setup here
     using namespace Enum;
     using Ports = std::vector<std::unique_ptr<Port> >;
-    Ports ports = EnumPortsInitRandom();
+    auto randomVector = setup();
+    Ports ports = EnumPortsInitRandom(randomVector);
     for (auto _ : state) {
         // This code gets timed
         benchmark::DoNotOptimize(ports.data());
@@ -215,7 +26,8 @@ static void BM_OopRandom(benchmark::State& state)
     // Perform setup here
     using namespace Oop;
     using Ports = std::vector<std::unique_ptr<Port> >;
-    Ports ports = OopPortsInitRandom();
+    auto randomVector = setup();
+    Ports ports = OopPortsInitRandom(randomVector);
     for (auto _ : state) {
         // This code gets timed
         benchmark::DoNotOptimize(ports.data());
@@ -231,7 +43,59 @@ static void BM_VisitorRandom(benchmark::State& state)
     // Perform setup here
     using namespace Visit;
     using Ports = std::vector<std::unique_ptr<Port> >;
-    Ports ports = VisitPortsInitRandom();
+    auto randomVector = setup();
+    Ports ports = VisitPortsInitRandom(randomVector);
+    for (auto _ : state) {
+        // This code gets timed
+        benchmark::DoNotOptimize(ports.data());
+        for (uint32_t i = 0; i < roundCount; ++i) {
+            writePorts(ports, 0xFF);
+            benchmark::ClobberMemory();
+        }
+    }
+}
+
+static void BM_StrategyRandom(benchmark::State& state)
+{
+    // Perform setup here
+    using namespace Strategy;
+    using Ports = std::vector<std::unique_ptr<Port> >;
+    auto randomVector = setup();
+    Ports ports = StrategyPortsInitRandom(randomVector);
+    for (auto _ : state) {
+        // This code gets timed
+        benchmark::DoNotOptimize(ports.data());
+        for (uint32_t i = 0; i < roundCount; ++i) {
+            writePorts(ports, 0xFF);
+            benchmark::ClobberMemory();
+        }
+    }
+}
+
+static void BM_VariantRandom(benchmark::State& state)
+{
+    // Perform setup here
+    using namespace VariantT;
+    using Ports = std::vector<Port>;
+    auto randomVector = setup();
+    Ports ports = VarianttPortsInitRandom(randomVector);
+    for (auto _ : state) {
+        // This code gets timed
+        benchmark::DoNotOptimize(ports.data());
+        for (uint32_t i = 0; i < roundCount; ++i) {
+            writePorts(ports, static_cast<uint32_t>(0xFF));
+            benchmark::ClobberMemory();
+        }
+    }
+}
+
+static void BM_TypeErasureRandom(benchmark::State& state)
+{
+    // Perform setup here
+    using namespace TypeErasureUp;
+    using Ports = std::vector<statable>;
+    auto randomVector = setup();
+    Ports ports = TypeErasureUpPortsInitRandom(randomVector);
     for (auto _ : state) {
         // This code gets timed
         benchmark::DoNotOptimize(ports.data());
@@ -243,14 +107,11 @@ static void BM_VisitorRandom(benchmark::State& state)
 }
 
 // Register the function as a benchmark
-// BENCHMARK(BM_Enum);
-// BENCHMARK(BM_Enum2);
-// BENCHMARK(BM_Enum3);
-// BENCHMARK(BM_EnumInterleave);
-// BENCHMARK(BM_EnumInterleave2);
-BENCHMARK(BM_Setup);
 BENCHMARK(BM_EnumRandom);
 BENCHMARK(BM_OopRandom);
 BENCHMARK(BM_VisitorRandom);
+BENCHMARK(BM_StrategyRandom);
+BENCHMARK(BM_VariantRandom);
+BENCHMARK(BM_TypeErasureRandom);
 // Run the benchmark
 BENCHMARK_MAIN();
