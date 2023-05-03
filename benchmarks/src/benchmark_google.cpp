3b57a596b4af46f50850f88a087d19f7318d1a11
Mark Tomm <mark.tomm@martem.ee>
2023-05-03 17:41:28 +0300

    duplicate all Y3 to benchmarks

--- benchmarks/src/benchmark_google.cpp
+++ benchmarks/src/benchmark_google.cpp
@@ -30,6 +30,18 @@ static void BM_B1_Enum(benchmark::State& state) {
     }
 }
 
+static void BM_Y3_EnumVectorGlobalItUpFnCall(benchmark::State& state) {
+    using namespace Enum;
+    using namespace enum_type;
+    auto intVec = GetGlobalRandIntVec();
+    auto v = EnumPortsInitRandom(intVec);
+    for (auto _ : state) {
+        auto p = v[GetNextGlobalIndex()].get();
+        writePort(p, BufferData{0xD});
+        benchmark::DoNotOptimize(p);
+    }
+}
+
 static void BM_B2_Oop(benchmark::State& state) {
     // Perform setup here
     using namespace oop;
@@ -110,6 +122,61 @@ static void BM_B6_TypeErasure(benchmark::State& state) {
     }
 }
 
+static void BM_Y3_OopVectorGlobalItUpFnCall(benchmark::State& state) {
+    using namespace oop;
+    auto intVec = GetGlobalRandIntVec();
+    auto v = OopPortsInitRandom(intVec);
+    for (auto _ : state) {
+        auto p = v[GetNextGlobalIndex()].get();
+        p->Write(BufferData{0x0D});
+        benchmark::DoNotOptimize(p);
+    }
+}
+
+static void BM_Y3_StrategyVectorGlobalItUpFnCall(benchmark::State& state) {
+    using namespace strategy;
+    auto intVec = GetGlobalRandIntVec();
+    auto v = StrategyPortsInitRandom(intVec);
+    for (auto _ : state) {
+        auto p = v[GetNextGlobalIndex()].get();
+        p->Write(BufferData{0x0D});
+        benchmark::DoNotOptimize(p);
+    }
+}
+
+static void BM_Y3_VisitorVectorGlobalItUpFnCall(benchmark::State& state) {
+    using namespace visitor;
+    auto intVec = GetGlobalRandIntVec();
+    auto v = VisitPortsInitRandom(intVec);
+    for (auto _ : state) {
+        auto p = v[GetNextGlobalIndex()].get();
+        p->accept(Write{BufferData{0x0D}});
+        benchmark::DoNotOptimize(p);
+    }
+}
+
+static void BM_Y3_VarianttVectorGlobalItUpFnCall(benchmark::State& state) {
+    using namespace variant_t;
+    auto intVec = GetGlobalRandIntVec();
+    auto v = VarianttPortsInitRandom(intVec);
+    for (auto _ : state) {
+        auto p = v[GetNextGlobalIndex()];
+        visit(Write{}, p, WriteData{static_cast<uint32_t>(0x0D)});
+        benchmark::DoNotOptimize(p);
+    }
+}
+
+static void BM_Y3_TypeErUpVectorGlobalItUpFnCall(benchmark::State& state) {
+    using namespace type_erasure_up;
+    auto intVec = GetGlobalRandIntVec();
+    auto v = TypeErasureUpPortsInitRandom(intVec);
+    for (auto _ : state) {
+        auto p = v[GetNextGlobalIndex()];
+        write(p, BufferData{0x0D});
+        benchmark::DoNotOptimize(p);
+    }
+}
+
 // Register the function as a benchmark
 BENCHMARK(BM_B1_Enum);
 BENCHMARK(BM_B2_Oop);
@@ -117,5 +184,11 @@ BENCHMARK(BM_B3_Visitor);
 BENCHMARK(BM_B4_Strategy);
 BENCHMARK(BM_B5_Variant);
 BENCHMARK(BM_B6_TypeErasure);
+BENCHMARK(BM_Y3_EnumVectorGlobalItUpFnCall);
+BENCHMARK(BM_Y3_OopVectorGlobalItUpFnCall);
+BENCHMARK(BM_Y3_StrategyVectorGlobalItUpFnCall);
+BENCHMARK(BM_Y3_VisitorVectorGlobalItUpFnCall);
+BENCHMARK(BM_Y3_VarianttVectorGlobalItUpFnCall);
+BENCHMARK(BM_Y3_TypeErUpVectorGlobalItUpFnCall);
 // Run the benchmark
 BENCHMARK_MAIN();
