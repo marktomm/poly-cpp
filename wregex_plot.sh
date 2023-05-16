0829d4a8fddde4774d73581efd2baaddeda7f579
Mark Tomm <mark.tomm@martem.ee>
2023-05-16 11:16:48 +0300

    normal wregex does not apply to "benchmarks" dir

--- wregex_plot.sh
+++ wregex_plot.sh
@@ -13,4 +13,4 @@ ARG1=${1}
 ARG1=$(trimFwdSlashes ${ARG1})
 ARG2=${2}
 
-./gen_bench.sh ${ARG1} && ./_build.sh && ./regex_plot.sh ${ARG1} ${ARG2} opt && ./regex_plot.sh ${ARG1} ${ARG2}
+./_build.sh && ./regex_plot.sh ${ARG1} ${ARG2} opt && ./regex_plot.sh ${ARG1} ${ARG2}
